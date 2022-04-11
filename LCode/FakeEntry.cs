3fd31d74c31562a9d1375851f73ca82afbc81781
Yang Lu <lu.yang.aus@gmail.com>
2022-04-12 04:00:19 +1000

    Add test ref

---
+++ LCode/FakeEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCode
+{
+    internal class FakeEntry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
