b3a95e4ff06890981449ea9d22ce2793da50eb7f
Yang Lu <yang.lu@dubber.net>
2022-06-06 16:45:09 +1000

    1.two-sum

---
+++ LCodeUnitTests/1.two-sum-tests.cs
@@ -0,0 +1,25 @@
+﻿using LCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LCodeUnitTests
+{
+    public class TwoSumTests
+    {
+        [Fact]
+        public void TestCases()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var expect = new int[] { 0, 1 };
+
+            var result1 = OneTwoSum.TwoSum(nums, target);
+
+            Assert.Equal(expect, result1);
+        }
+    }
+}
