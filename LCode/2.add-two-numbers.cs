243908b51e4b8ac2ce86c4f433826d187a87c65e
Yang Lu <yang.lu@dubber.net>
2022-06-17 16:05:14 +1000

    put listNode in a new class to fix codefactor

--- LCode/2.add-two-numbers.cs
+++ LCode/2.add-two-numbers.cs
@@ -1,23 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LCode._2;
 
 namespace LCode
 {
-    public partial class ListNode
-    {
-        public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
-    public partial class AddTwoNumbersSolution
+    public class AddTwoNumbersSolution
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
--- LCodeUnitTests/2.AddTwoNumbersTest.cs
+++ LCodeUnitTests/2.AddTwoNumbersTest.cs
@@ -1,4 +1,5 @@
 ﻿using LCode;
+using LCode._2;
 using System;
 using System.Collections.Generic;
 using System.Linq;
