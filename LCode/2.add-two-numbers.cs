9d07c3b188f82ea1da5b4ea58a046f1015223461
Yang Lu <yang.lu@dubber.net>
2022-06-17 15:39:54 +1000

    Add 2 numbers and tests

---
+++ LCode/2.add-two-numbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class AddTwoNumbersSolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var result = 0;
+            ListNode current = new ListNode();
+            ListNode head = new ListNode();
+            head.next = current;
+            do
+            {
+                var l1Value = l1?.val ?? 0;
+                var l2Value = l2?.val ?? 0;
+
+                result += l1Value + l2Value;
+
+                var lastPosition = result % 10;
+                current.val = lastPosition;
+                ListNode temp;
+                if (l1?.next is not null || l2?.next is not null || result > 9)
+                {
+                    temp = new ListNode();
+                    current.next = temp;
+                    current = temp;
+                }
+
+                l1 = l1?.next ?? null;
+                l2 = l2?.next ?? null;
+
+                result = result / 10;
+            } while (l1 is not null || l2 is not null || result > 0);
+
+            return head.next;
+        }
+    }
+}
---
+++ LCodeUnitTests/a2AddTwoNumbersTest.cs
@@ -0,0 +1,57 @@
+﻿using LCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LCodeUnitTests
+{
+    public class a2AddTwoNumbersTest
+    {
+        [Fact]
+        public void AddTwoNumbers_Test_OverInt() 
+        {
+            // Arrange
+            ListNode listNode1 = new ListNode(9);
+            ListNode listNode2 = new ListNode(1);
+
+            ListNode lastNode = listNode2;
+            for (var i = 0; i < 9; i++)
+            {
+                var current = new ListNode(9);
+                lastNode.next = current;
+                lastNode = current;
+            }
+            ListNode expectCurrent;
+
+            // Action
+            AddTwoNumbersSolution solution = new AddTwoNumbersSolution();
+            var result = solution.AddTwoNumbers(listNode1, listNode2);
+
+            // Assert
+            expectCurrent = result;
+            for (int index = 0; index < 11; index++)
+            {
+                if (index != 10)
+                    Assert.Equal(0,expectCurrent.val);
+                else
+                    Assert.Equal(1,expectCurrent.val);
+
+                expectCurrent = expectCurrent.next;
+            }
+        }
+
+        [Fact]
+        public void AddTwoNumbers_Test_JinWei()
+        { 
+            var listNode1 = new ListNode(9);
+           var listNode2 = new ListNode(1);
+
+            var result = new AddTwoNumbersSolution().AddTwoNumbers(listNode1, listNode2);
+            Assert.Equal(0,result.val);
+            Assert.Equal(1,result.next.val);
+        }
+    }
+}
