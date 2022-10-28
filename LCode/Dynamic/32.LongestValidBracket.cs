b8e4a54c47573a182f5e967fa2ae39ee5b49bfb8
Yang Lu <lu.yang.aus@gmail.com>
2022-10-28 15:14:55 +1100

    add 32 最长合法括号

---
+++ LCode/Dynamic/32.LongestValidBracket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LCode.Dynamic;
+
+public class LongestValidBracket
+{
+    public int LongestValidParentheses(string s)
+    {
+        var max = 0;
+        Stack<int> stack = new Stack<int>();
+        stack.Push(-1);
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(')
+            {
+                stack.Push(i);
+            }
+            else
+            {
+                stack.Pop();
+                if (stack.Count == 0)
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    max = Math.Max(max, i - stack.Peek());
+                }
+            }
+        }
+
+        return max;
+    }
+}
