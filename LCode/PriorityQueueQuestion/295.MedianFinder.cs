3df8d0072ea82f85a962799631b417388cc0e037
Yang Lu <lu.yang.aus@gmail.com>
2022-10-28 16:32:30 +1100

    add 295 中位数

---
+++ LCode/PriorityQueueQuestion/295.MedianFinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LCode.PriorityQueueQuestion;
+
+public class MedianFinder
+{
+    private PriorityQueue<int, int> queueMin;
+    private PriorityQueue<int, int> queueMax;
+
+    public MedianFinder()
+    {
+        queueMin = new PriorityQueue<int, int>();
+        queueMax = new PriorityQueue<int, int>();
+    }
+
+    public void AddNum(int num)
+    {
+        if (queueMin.Count == 0 || queueMin.Peek() >= num)
+        {
+            queueMin.Enqueue(num, -num);
+        }
+        else
+        {
+            queueMax.Enqueue(num, num);
+        }
+
+        if (queueMin.Count < queueMax.Count)
+        {
+            var temp = queueMax.Dequeue();
+            queueMin.Enqueue(temp, -temp);
+        }
+
+        if (queueMin.Count > queueMax.Count + 1)
+        {
+            var temp = queueMin.Dequeue();
+            queueMax.Enqueue(temp, temp);
+        }
+    }
+
+    public double FindMedian()
+    {
+        if (queueMax.Count == queueMin.Count)
+        {
+            return (queueMax.Peek() + queueMin.Peek()) / 2.0;
+        }
+
+        return queueMin.Peek();
+    }
+}
